4df06f85f9457b33c68892903f0d7af8828637f6
Aswin-5 <108947806+Aswin-5@users.noreply.github.com>
2022-07-09 18:46:51 +0530

    changed

---
+++ src/test1/Test1.java
@@ -0,0 +1,95 @@
+
+package test1;
+
+import java.awt.Desktop;
+import java.net.URI;
+
+public class Test1 {
+
+
+    public static void main(String[] args) throws Exception {
+        Desktop desk = Desktop.getDesktop();
+        desk.browse(new URI("https://youtu.be/dQw4w9WgXcQ"));
+    }
+  
+}
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+//        String num = c.nextLine();
+//            
+//        String x = num.replace(" ", "");
+//        
+//        int i = Integer.parseInt(x);
+//        int result = i+260;
+//        
+//        System.out.println(result/100+" "+result%100);
+//        
+//        
+//    }
+//    {
+                   
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+//        String num = c.nextLine();
+//            
+//        String x = num.replace(" ", "");
+//        
+//        int i = Integer.parseInt(x);
+//        int result = i+260;
+//        
+//        System.out.println(result/100+" "+result%100);
+//        
+//        
+//    }
+//    
+//}
