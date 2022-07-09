b14582ec2903a09456138050e4a4539cf2142ba8
Aswin-5 <108947806+Aswin-5@users.noreply.github.com>
2022-07-09 14:02:41 +0530

    added today

--- Firstcode.java
+++ Firstcode.java
@@ -3,7 +3,7 @@ package firstcode;
 
 public class Firstcode {
     public static void main(String[] args) {
-        System.out.println("#changed-first code git!!");
+        System.out.println("#changed-today");
     }
     
 }
